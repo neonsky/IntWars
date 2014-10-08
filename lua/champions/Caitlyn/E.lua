6b4a231c6c8f09f7505953b2ea8d538a6e4826a3
H3npi <Henry@voss-webmail.de>
2014-10-08 19:52:39 +0200

    Update Caitlyn E
    added basic code,
    cleaned up a bit

--- lua/champions/Caitlyn/E.lua
+++ lua/champions/Caitlyn/E.lua
@@ -1,22 +1,47 @@
 Vector2 = require 'Vector2' -- include 2d vector lib 
 
 function finishCasting()
+    -- Knockback Start
+
+    
     local current = Vector2:new(getOwnerX(), getOwnerY())
-    local to = Vector2:new(getSpellToX(), getSpellToY())
+    local to = Vector2:new(0-getSpellToX(), 0-getSpellToY())
 
-    to:sub(current)
-    to:normalize()
+    to:add(current)
+    local trueCoords
 
-    local teleportTo = Vector2:new(to.x)
-    teleportTo:mul(400)
+    trueCoords = Vector2:new(current.x, current.y)
 
-    local trueCoords = Vector2:new(teleportTo.x, teleportTo.y)
-    trueCoords:add(current)
+
+    if(to:length() > 400) then
+        to:normalize()
+        local range = Vector2:Mult(to,400)
+        trueCoords = Vector2:new(current.x, current.y)
+        trueCoords:add(range)
+    elseif(to:length() >= 237.5 and isWalkable(getSpellToX(), getSpellToY()) == false) then
+        -- Apply position fix when requested teleport distance is more than half
+        to:normalize()
+        local range = Vector2:Mult(to,475)
+        trueCoords = Vector2:new(current.x, current.y)
+        trueCoords:add(range)
+    else
+        trueCoords = Vector2:new(getSpellToX(), getSpellToY())
+    end
 
     teleportTo(trueCoords.x, trueCoords.y)
-    addProjectile(getSpellToX(), getSpellToY())
+    -- Knockback End
+    -- Projectile Start
+    local current = Vector2:new(getOwnerX(), getOwnerY())
+    local to = Vector2:new(getSpellToX(), getSpellToY())
+    to:add(current)
+    to.normalize()
+    local range = Vector2:Mult(to, 1000)
+    local trueCoords = Vector2:add(current, range)
+    addServerProjectile(trueCoords.x, trueCoords.y)
+    -- Projectile End
 end
 
 function applyEffects()
     --todo slow and damage enemy
+    addParticleTarget("caitlyn_entrapment_cas.troy", getOwner())
 end
