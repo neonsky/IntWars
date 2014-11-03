0cd74ef62edb92272ae273a699e19a5681981a59
Querijn Heijmans <querijn@irule.at>
2014-11-03 16:22:24 +0100

    Fixed map width size.
    - AIMesh reported an incorrect width, is now correct.
    - It now converts it correctly to a MovementVector position.

--- gamed/src/AIMesh.cpp
+++ gamed/src/AIMesh.cpp
@@ -111,8 +111,11 @@ bool AIMesh::outputMesh(unsigned width, unsigned height)
 			highY = fileStream->triangles[i].Face.v3[2];
 	}
 
-   mapWidth = (highX - lowX);
-   mapHeight = (highY - lowY);
+
+   mapWidth = (highX + lowX);
+   mapHeight = (highY + lowY);
+
+   printf("High: %f, %f\n", mapWidth, mapHeight);
    
    // If the width or width larger?
    if ((highY - lowY) < (highX - lowX))
