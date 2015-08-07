95749c7b6c6243dc625192ad41c3f85cf46c7c3f
neonsky <bnnb@online.de>
2015-08-07 12:32:08 +0200

    deleted dumbs

--- dep/src/raf/RAFManager.cpp
+++ dep/src/raf/RAFManager.cpp
@@ -2,6 +2,7 @@
 #include "tinydir.h"
 #include "stdafx.h"
 #include "Logger.h"
+#include <algorithm>
 
 
 #ifdef _WIN32
@@ -68,95 +69,8 @@ bool RAFManager::readFile(const std::string& path, vector<unsigned char>& toFill
 }
 std::string RAFManager::findGameBasePath()
 {
-	//CORE_INFO("Searching for LoL base path...");
-#ifdef _WIN32
-	HKEY hKey;
-	std::vector<string> strKeyPathCU, strKeyPathLM;
-	strKeyPathCU.push_back("SOFTWARE\\RIOT GAMES\\RADS"); 
-	strKeyPathCU.push_back("VirtualStore\\MACHINE\\SOFTWARE\\Wow6432Node\\RIOT GAMES\\RADS");
-	strKeyPathCU.push_back("SOFTWARE\\Classes\\VirtualStore\\MACHINE\\SOFTWARE\\Wow6432Node\\RIOT GAMES\\RADS");
-	strKeyPathCU.push_back("SOFTWARE\\Classes\\VirtualStore\\MACHINE\\SOFTWARE\\RIOT GAMES\\RADS");
-
-	strKeyPathLM.push_back("Software\\Wow6432Node\\Riot Games\\RADS");
-	strKeyPathLM.push_back("SOFTWARE\\RIOT GAMES\\RADS");
-
-	string strKeyName = "LOCALROOTFOLDER";
-	DWORD dwValueType;
-	TCHAR byteValue[100];
-	DWORD dwValueSize;
-
-
-   //Check CLASSES_ROOT keys
-   for (size_t i = 0; i < strKeyPathCU.size(); i++)
-   {
-      if (RegOpenKeyExA(HKEY_CLASSES_ROOT, strKeyPathCU[i].c_str(), 0, KEY_ALL_ACCESS, &hKey) != ERROR_SUCCESS)
-      {
-         //CORE_WARNING("Cannot open key HKEY_CLASSES_ROOT '%s'", strKeyPathCU[i].c_str());
-         continue;
-      }
-
-      if (RegQueryValueExA(hKey, strKeyName.c_str(), NULL, &dwValueType, (LPBYTE)byteValue, &dwValueSize) != ERROR_SUCCESS)
-      {
-         //CORE_WARNING("Cannot read key HKEY_CLASSES_ROOT '%s'", strKeyPathCU[i].c_str());
-         continue;
-      }
-
-      string sValue(byteValue);
-      sValue += "/projects/lol_game_client/";
-      CORE_INFO("Found base path in %s", sValue.c_str());
-
-      return sValue;
-   }
-
-   //Check CURRENT_USER keys
-   for (size_t i = 0; i < strKeyPathCU.size(); i++)
-   {
-      if (RegOpenKeyExA(HKEY_CURRENT_USER, strKeyPathCU[i].c_str(), 0, KEY_ALL_ACCESS, &hKey) != ERROR_SUCCESS)
-      {
-         //CORE_WARNING("Cannot open key HKEY_CURRENT_USER '%s'", strKeyPathCU[i].c_str());
-         continue;
-      }
-
-      if (RegQueryValueExA(hKey, strKeyName.c_str(), NULL, &dwValueType, (LPBYTE)byteValue, &dwValueSize) != ERROR_SUCCESS)
-      {
-         //CORE_WARNING("Cannot read key HKEY_CURRENT_USER '%s'", strKeyPathCU[i].c_str());
-         continue;
-      }
-
-      string sValue(byteValue);
-      sValue += "/projects/lol_game_client/";
-      CORE_INFO("Found base path in %s", sValue.c_str());
-
-      return sValue;
-   }
-
-	//Check LOCAL_MACHINE keys
-
-	for(size_t i=0; i< strKeyPathLM.size();i++)
-   {
-      if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, strKeyPathLM[i].c_str(), 0, KEY_ALL_ACCESS, &hKey) != ERROR_SUCCESS)
-      {
-         //CORE_WARNING("Cannot open key HKEY_LOCAL_MACHINE '%s'", strKeyPathCU[i].c_str());
-         continue;
-      }
-
-		if( RegQueryValueExA(hKey, strKeyName.c_str(), NULL, &dwValueType, (LPBYTE)byteValue, &dwValueSize) != ERROR_SUCCESS )
-      {
-         //CORE_WARNING("Cannot read key HKEY_LOCAL_MACHINE '%s'", strKeyPathCU[i].c_str());
-			continue;
-		}
-
-		std::string sValue(byteValue);
-		sValue += "/projects/lol_game_client/";
-
-		CORE_INFO("Found base path in %s",sValue.c_str());
-		return sValue;
-	}
-
-	CORE_ERROR("Couldnt find League of Legends game path or unable to read Registry keys");
-	return "";
-
-#else
-	return "";
-#endif
+   string sValue = "D:\\code\\IntWars\\League of Legends\\RADS\\projects\\lol_game_client\\";
+   std::replace(sValue.begin(), sValue.end(), '\\', '/');
+   CORE_INFO("LOL PATH: %s", sValue.c_str());
+   return sValue;
 }
